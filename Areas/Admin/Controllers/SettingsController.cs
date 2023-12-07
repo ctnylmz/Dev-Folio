a04d15b1f2896084ea8e3b4796c2639755ee303c
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2023-12-07 20:43:23 +0300

    Create Settings Page

--- Areas/Admin/Controllers/SettingsController.cs
+++ Areas/Admin/Controllers/SettingsController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Dev_Folio.Areas.Admin.Models;
+using Dev_Folio.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dev_Folio.Areas.Admin.Controllers
 {
@@ -7,10 +11,50 @@ namespace Dev_Folio.Areas.Admin.Controllers
     [Authorize]
     public class SettingsController : Controller
     {
-        [Route("Admin/Settings")]
-        public IActionResult Index()
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly DevFolioContext _context;
+
+        public SettingsController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, DevFolioContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        [Route("/Admin/Settings")]
+        public IActionResult Settings()
         {
             return View();
+        }
+
+        [Route("/Admin/Settings")]
+        [HttpPost]
+        public async Task<IActionResult> Settings(Password password)
+        {
+            if (ModelState.IsValid)
+            {
+                if (password.NewPassword == password.ConfirmPassword)
+                {
+                    var user = await _userManager.GetUserAsync(User);
+                    var changePasswordResult = await _userManager.ChangePasswordAsync(user, password.currentPassword, password.NewPassword);
+                    if (changePasswordResult.Succeeded)
+                    {
+                        return RedirectToAction("Index","Admin");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("currentPassword", "Eski Şifre Hatalı");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("currentPassword", "Yeni Şife ve Tekrar Şifre Hatalı");
+                }
+            }
+
+            return View();
+
         }
     }
 }
---
+++ Areas/Admin/Models/Password.cs
@@ -0,0 +1,9 @@
+﻿namespace Dev_Folio.Areas.Admin.Models
+{
+    public class Password
+    {
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string currentPassword { get; set; }
+    }
+}
